0adf736882ee695ced6a546bc0891bc19605fa0f
nicholaschangIT <nicholasc001@hotmail.com>
2022-09-19 16:11:33 +0100

    updated terraform code

--- strorageaccount/provider.tf
+++ strorageaccount/provider.tf
@@ -9,6 +9,5 @@ terraform {
       storage_account_name = "saterraformdevops"
       container_name = "terraformdemo"
       key            = "dev.terraform.tfstate"
-      access_key = "1juF/1zlXgQpApvX2HQBKJRu2AgxJt1V4jefJ6LdoUFJWHmEJljbRnr7LJwjT0eKWlG4UWJ2vqTR+AStgYovDw=="
     }
 }
